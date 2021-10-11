fd562cbd423a811c983f99240970415fcffd8ac7
Yuliya Grigoreva <julija.grigoreva.01@privatbank.ua>
2021-10-11 19:18:12 +0300

    init commit

---
+++ src/com/pb/grigoreva/hw1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.pb.grigoreva.hw1;
+
+import sun.awt.geom.AreaOp;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
