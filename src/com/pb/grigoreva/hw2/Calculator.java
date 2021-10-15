edbfc5f45be5778985959cbd6105f7995fd07f10
Yuliya Grigoreva <julija.grigoreva.01@privatbank.ua>
2021-10-15 13:48:08 +0300

    init commit

---
+++ src/com/pb/grigoreva/hw2/Calculator.java
@@ -0,0 +1,33 @@
+package com.pb.grigoreva.hw2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        // char grade = args [0] .charAt (0);
+        char num = 'D';
+        Scanner scan = new Scanner(System.in);
+        int x ;
+        int y;
+        x = scan.nextInt() ;
+        y = scan.nextInt() ;
+        switch (num) {
+            case  'A' :
+                System.out.println( +(x+y));
+                break;
+            case  'B' :
+                System.out.println( +(x-y));
+                break;
+            case  'C' :
+                System.out.println( +(x*y));
+                break;
+            case  'D' :
+                System.out.println( +(x/y));
+                if (y==0) {
+                    System.out.println( "делить на ноль нельзя");}
+                    break;
+
+                }
+
+    }
+}
