64c8f2773f30bd19ff34c432e377cdf76e767f12
Yuliya Grigoreva <julija.grigoreva.01@privatbank.ua>
2021-10-23 22:19:19 +0300

    init commit

---
+++ src/com/pb/grigoreva/hw3/Bingo.java
@@ -0,0 +1,44 @@
+package com.pb.grigoreva.hw3;
+
+import java.util.Scanner;
+
+public class Bingo {
+    public static void main(String[] args) {
+        System.out.println("Угадайте целое число в диапазоне от 0 до 100 с пяти попыток.");
+        System.out.println("Для выхода из программы введите - exit.");
+
+        final int MAX_ATTEMPT = 5;
+        int attempt = 0;
+        String x = "8";
+        int y;
+        Scanner in = new Scanner(System.in);
+
+        while (attempt < MAX_ATTEMPT) {
+            attempt++;
+
+            System.out.println("Попытка " + attempt + ":");
+            String value = in.next();
+            y = Integer.parseInt(value);
+            if (y > 8) {
+                System.out.println("y>8");
+            }else if (y < 8) {
+                System.out.println("y<8");
+            } else if (y ==8)
+            {System.out.println("y=8");}
+
+
+                if (value.equals("exit")) {
+                    break;
+
+                }
+
+                if (!value.equals(x)) {
+                    continue;
+                }
+
+                System.out.println("Поздравляем, Вы угадали с " + attempt + " попытки!");
+                break;
+            }
+
+        }
+    }
