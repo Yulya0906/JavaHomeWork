0dbae0ec7e4fb5dc185bf588bdd15a46fad3b051
Yuliya Grigoreva <julija.grigoreva.01@privatbank.ua>
2021-11-28 19:51:42 +0200

    init commit

--- src/com/pb/grigoreva/hw9/FileNumbers.java
+++ src/com/pb/grigoreva/hw9/FileNumbers.java
@@ -9,7 +9,7 @@ public class FileNumbers {
             int[][] array = new int[10][10];
             for (int i = 0; i < array.length; i++, System.out.println()) {
                 for (int j = 0; j < array.length; j++) {
-                    array[i][j] = ((int) (Math.random() * 99+ 1));
+                    array[i][j] = ((int) (Math.random() * (99+ 1)));
                     System.out.print(array[i][j] + "");
                     writer.write(array[i][j] + "");
                 }
